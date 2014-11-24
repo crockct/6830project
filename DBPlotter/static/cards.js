b725ee004bf4f63eb5f2cd70013807bb834a7081
crockct <crockct@mit.edu>
2014-11-24 13:01:46 -0500

    updated chartJS file with one method for each charttype

--- DBPlotter/static/cards.js
+++ DBPlotter/static/cards.js
@@ -1,5 +1,6 @@
 
 var myPieChart;
+var myHistogramChart;
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
@@ -10,7 +11,7 @@ function getRandomColor() {
     return color;
 }
 
-function runQuery() {
+function runQuery(chartType) {
     
     document.getElementById("execute-button").innerHTML = '<i class="fa fa-spinner"></i>';
 
@@ -38,6 +39,85 @@ function runQuery() {
 
 }
 
-function editQuery() {
-    document.querySelector(".flipper").classList.toggle("flip")
+function runQueryPieChart(){
+	document.getElementById("execute-button").innerHTML = '<i class="fa fa-spinner"></i>';
+
+    $.ajax({
+	dataType: "json",
+	url: "http://127.0.0.1:8000/pie_chart/?d=".concat($('#dbfile').val()).concat("&q=").concat($('#query').val().replace(/\n/g, " ").replace(";", "")),
+	success: function(data)
+	{
+	    var pie_data = [];
+
+	    $.each( data, function(key, val) {
+		pie_data.push({value: val, label:key, color: getRandomColor()});
+	    });
+
+	    document.getElementById("execute-button").innerHTML = '<i class="fa fa-line-chart"></i>';
+	    //document.getElementById("results-title-span").textContent = 'Pie Chart of Results';
+
+	    document.querySelector(".flipper").classList.toggle("flip")
+	    var canvas = document.getElementById("myPieChart");
+	    var ctx = canvas.getContext("2d");
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	    myPieChart = new Chart(ctx).Pie(pie_data,{});
+	}
+    });
+}
+
+function runQueryWordCloud(){
+	document.getElementById("execute-button").innerHTML = '<i class="fa fa-spinner"></i>';
+
+    $.ajax({
+	dataType: "json",
+	url: "http://127.0.0.1:8000/pie_chart/?d=".concat($('#dbfile').val()).concat("&q=").concat($('#query').val().replace(/\n/g, " ").replace(";", "")),
+	success: function(data)
+	{
+	    var pie_data = [];
+
+	    $.each( data, function(key, val) {
+		pie_data.push({value: val, label:key, color: getRandomColor()});
+	    });
+
+	    document.getElementById("execute-button").innerHTML = '<i class="fa fa-line-chart"></i>';
+	    //document.getElementById("results-title-span").textContent = 'Pie Chart of Results';
+
+	    document.querySelector(".flipper").classList.toggle("flip")
+	    var canvas = document.getElementById("myWordCloud");
+	    var ctx = canvas.getContext("2d");
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	    myPieChart = new Chart(ctx).Pie(pie_data,{});
+	}
+    });
+}
+
+function runQueryHistogram(){
+	document.getElementById("execute-button").innerHTML = '<i class="fa fa-spinner"></i>';
+
+    $.ajax({
+	dataType: "json",
+	url: "http://127.0.0.1:8000/pie_chart/?d=".concat($('#dbfile').val()).concat("&q=").concat($('#query').val().replace(/\n/g, " ").replace(";", "")),
+	success: function(data)
+	{
+	    var hist_data = [];
+
+	    $.each( data, function(key, val) {
+		hist_data.push({value: val, label:key, color: getRandomColor()});
+	    });
+
+	    document.getElementById("execute-button").innerHTML = '<i class="fa fa-line-chart"></i>';
+	    //document.getElementById("results-title-span").textContent = 'Pie Chart of Results';
+
+	    document.querySelector(".flipper").classList.toggle("flip")
+	    var canvas = document.getElementById("myHistogram");
+	    var ctx = canvas.getContext("2d");
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	    myHistogramChart = new Chart(ctx).Bar(hist_data,{});
+	}
+    });
+}
+
+function flipCard() {
+    $(".flipper").toggleClass("flip");
+	
 }
