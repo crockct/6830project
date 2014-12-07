aa9653c66334e5cdeeb40880b260860b33ead8d1
Isaac Evans <isaac@spoon.net>
2014-12-07 14:53:28 -0500

    beginning work the UI for a table--when you select a table to view cards for

--- DBPlotter/static/index.js
+++ DBPlotter/static/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     filepathBtnEvent();
+    tableNameBtnEvent();
 })
 
 var filepathBtnEvent = function() {
@@ -13,11 +14,34 @@ var filepathBtnEvent = function() {
             },
             success: function(data) {
                 table_names = [];
-                for (i in data){
-                    table_names.push('<li><a href="yourlink?id=' + data[i] + '">' + data[i] + '</a></li>');
+                table_names.push('<li><a class="tablename">' + "tablename1" + '</a></li>');
+                for (i in data) {
+                    table_names.push('<li><a href="tablecard?id=' + data[i] + '">' + data[i] + '</a></li>');
                 }
                 $('#tables-list').html(table_names.join(''));
             }
         })
     });
+}
+
+var tableNameBtnEvent = function() {
+    $(".tablename").on("click", function(e) {
+    	e.preventDefault();
+    	// get info needed to populate pie chart card
+    	$.ajax({
+            type: "GET",
+            url: "/tablecard/",
+            data: {
+                "tablename": $(this).text()
+            },
+            success: function(data) {
+                table_names = [];
+                table_names.push('<li><a class="tablename" href="table?id=' + "tablename1" + '">' + "tablename1" + '</a></li>');
+                for (i in data) {
+                    table_names.push('<li><a href="table?id=' + data[i] + '">' + data[i] + '</a></li>');
+                }
+                $('#tables-list').html(table_names.join(''));
+            }
+        })
+    })
 }
